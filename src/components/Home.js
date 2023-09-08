3810bbde6a163aa067aa427a626ac4d1a872cf9e
4FunkE <4FunkEcodes@gmail.com>
2023-09-08 12:28:14 -0400

    scaffoled the home Page

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css'; // Import your CSS for styling
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="background-image"></div>
+      <div className="content">
+        <h1>Emily Rose Funk</h1>
+        <p>Funk.E_Art Designs</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
