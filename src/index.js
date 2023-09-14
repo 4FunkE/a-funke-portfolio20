19a1f9d95a587a4c8928ee532430e3783812f368
4FunkE <4FunkEcodes@gmail.com>
2023-09-14 19:57:17 -0400

    debugging issue with new version VS old version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css'; // Import your global CSS styles
 import App from './App'; // Import your main application component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  
-  document.getElementById('root')
+  </Router>,
 );
