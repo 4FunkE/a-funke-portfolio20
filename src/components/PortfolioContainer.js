e88bbc58dfd0ac3f31f9d319860fe096b6f5c61c
4FunkE <4FunkEcodes@gmail.com>
2023-09-13 21:30:04 -0400

    debug the portfolio contaner using activity from class

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,6 +1,5 @@
 //this is the portfolio container
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState } from 'react';
 import '../styles/PortfolioContainer.css';
 
 // Import all components
@@ -11,19 +10,34 @@ import Works from './Works';
 import Contact from './Contact';
 import Footer from './Footer';
 
-function App() {
-  return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <Route path="/" component={Home} />
-        <Route path="/aboutMe" component={AboutMe} />
-        <Route path="/Works" component={Works} />
-        <Route path="/contact" component={Contact} />
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
 
-export default App;
+ // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+ const renderPage = () => {
+  if (currentPage === 'Home') {
+    return <Home />;
+  }
+  if (currentPage === 'AboutMe') {
+    return <AboutMe />;
+  }
+  if (currentPage === 'Works') {
+    return <Works />;
+  }
+  if (currentPage === 'Contact') {
+    return <Contact />;
+  }
+  return <Footer />;
+};
+
+const handlePageChange = (page) => setCurrentPage(page);
+
+return (
+  <div>
+    {/* We are passing the currentPage from state and the function to update it */}
+    <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+    {/* Here we are calling the renderPage method which will return a component  */}
+    {renderPage()}
+  </div>
+);
+}
