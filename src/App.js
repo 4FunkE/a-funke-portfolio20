766c80b1da5d7cff31d407478b11d78d4aa0bfa4
4FunkE <4FunkEcodes@gmail.com>
2023-09-14 20:57:36 -0400

    debugging links. They are not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/Footer";
+// import AboutMe from "./components/AboutMe";
 import "./App.css";
 
 function App() {
     return (
       <div className="App">
         <PortfolioContainer />;
+        {/* <AboutMe /> */}
         <Footer />
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,63 +1,103 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Header.css';
+import React, { useState, useEffect } from "react";
+import "../styles/Header.css";
 
-function Header({ currentPage }) {
+function Header() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  useEffect(() => {
+    // Handle initial route based on window location
+    handleRouteChange(window.location.pathname);
+
+    // Listen for route changes and update currentPage
+    const handlePopState = () => {
+      handleRouteChange(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  const handleRouteChange = (route) => {
+    // Update currentPage based on route
+    switch (route) {
+      case "/":
+        setCurrentPage("Home");
+        break;
+      case "/aboutMe":
+        setCurrentPage("About");
+        break;
+      case "/works":
+        setCurrentPage("Works");
+        break;
+      case "/contact":
+        setCurrentPage("Contact");
+        break;
+      case "/resume":
+        setCurrentPage("Resume");
+        break;
+      default:
+        setCurrentPage("Home"); // Set a default page for unknown routes
+    }
+  };
+
   return (
     <header>
       <div className="logo">
-        <img 
-        src="../../public/Funk.E_ArtLogo" 
-        alt="Logo"
-        style={{ width: '100px', height: '50px' }}
-         />
+        <img
+          src='./Funk.E_ArtLogo'
+          alt="Funk.E_ArtLogo"
+          style={{ width: "100px", height: "50px" }}
+        />
       </div>
-    
-<nav>
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
-          to="/"
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/aboutMe"
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/works"
-          className={currentPage === 'Works' ? 'nav-link active' : 'nav-link'}
-        >
-          Works
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact"
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/resume"
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-    </ul>
-</nav>
-</header>
+
+      <nav>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a
+              href="/"
+              className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+            >
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="/aboutMe"
+              className={currentPage === "About" ? "nav-link active" : "nav-link"}
+            >
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="/works"
+              className={currentPage === "Works" ? "nav-link active" : "nav-link"}
+            >
+              Works
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="/contact"
+              className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="/resume"
+              className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
