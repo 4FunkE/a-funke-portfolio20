c8c163b11ed9ad0cb746b4cdd970f9af4de02bd9
4FunkE <4FunkEcodes@gmail.com>
2023-09-18 20:38:57 -0400

    style footer, rid links, scroll up button

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
 
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <footer>
-      <nav>
-        <ul className="nav nav-tabs">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/aboutMe" className="nav-link">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/works" className="nav-link">
-              Works
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <p>funk.E Codes</p>
+    <footer className="footer">
+      <button className="scroll-to-top-button" onClick={scrollToTop}>
+        Scroll to Top
+      </button>
+      <p class='p'>Funk.E_Codes</p>
     </footer>
   );
 }
